7a10f8f2bc12798be212a05a7d5d8d9cee71ce3e
subugee <sasubramanian_md@hotmail.com>
2024-09-25 11:21:25 +0530

    redux

---
+++ ui-react/react-reduxapp/src/main-redux-basic.jsx
@@ -0,0 +1,68 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+
+//redux : reducer (initalState,actionObject)
+const CounterReducer = (counter = 10, action) => {
+  //methods
+  switch (action.type) {
+    case 'counter/increment':
+      return counter + 1
+    default:
+      //default state
+      return counter;
+  }
+}
+
+//create store object
+const appStore = configureStore({
+  reducer: {
+    counter: CounterReducer
+  }
+})
+///////////////////////////////////////////////////////////////////////////////////////
+//React component layer
+const Counter = (props) => {
+  const counter = useSelector(state => {
+    //appState.reducerName
+    return state.counter
+  })
+
+  const dispatch = useDispatch()
+
+  const onIncrement = () => {
+    //action object
+    const incrementAction = {
+      type: 'counter/increment'
+    }
+    //dispatch an action to reducer
+    dispatch(incrementAction)
+  }
+  return <div>
+    <h1>React Redux</h1>
+    <h2>Counter {counter}</h2>
+    <button onClick={onIncrement}>+</button>
+  </div>
+}
+
+
+
+///
+//connecting react with redux: That should be top level component.
+
+//react-redux lib provides a context object through which we can pass store object into
+//react components
+const App = () => {
+  return <Provider store={appStore}>
+    {/* Your components */}
+    <Counter />
+  </Provider>
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
